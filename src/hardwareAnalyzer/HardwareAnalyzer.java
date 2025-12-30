db968068ac5593b48760be7fa24b8f672e879206
sqtwix <ivan20140767@gmail.com>
2025-12-30 21:28:49 +0300

    feat: new hardwareAnalyzer

---
+++ src/hardwareAnalyzer/HardwareAnalyzer.java
@@ -0,0 +1,9 @@
+package hardwareAnalyzer;
+
+import java.util.Properties;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+
+public class HardwareAnalyzer {
+
+}
