ebeee26c3f2e25b458b153da7e221ef729e833e6
sqtwix <ivan20140767@gmail.com>
2025-12-31 16:33:54 +0300

    feat: realization of all methods in main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
 
+import hardwareAnalyzer.*;
+
+public final String CYAN = "\u001B[36m";
+public final String BLUE = "\u001B[34m";
+public static final String RESET = "\u001B[0m";
+
+
+void main() {
+    printHeadLabel();
+
+    HardwareAnalyzer analyzer = new HardwareAnalyzer();
+
+    analyzer.printBasicSystemInfo();
+    analyzer.printCpuInfo();
 }
 
-public void printHeadLabel(){
-
+public void printHeadLabel() {
+    System.out.println(CYAN + "    ===SysInfo===" + RESET);
 }
--- src/hardwareAnalyzer/HardwareAnalyzer.java
+++ src/hardwareAnalyzer/HardwareAnalyzer.java
@@ -11,26 +11,30 @@ public class HardwareAnalyzer {
         Properties props = System.getProperties();
         Runtime runtime = Runtime.getRuntime();
 
+        printLine();
         System.out.println("===BASIC SYSTEM INFO===");
         System.out.println("Operation system: " + props.getProperty("os.name"));
-        System.out.println("OS version" + props.getProperty("os.version"));
-        System.out.println("OS architecture" + props.getProperty("os.arch"));
+        System.out.println("OS version: " + props.getProperty("os.version"));
+        System.out.println("OS architecture: " + props.getProperty("os.arch"));
 
         System.out.printf("\nUser name: " + props.getProperty("user.name") + "\n");
-        System.out.printf("User dir: " + props.getProperty("user.dir"));
+        System.out.printf("User dir: " + props.getProperty("user.dir") + "\n");
         System.out.println("User country: " + System.getProperty("user.country"));
 
         System.out.printf("\nJava runtime name: " + System.getProperty("java.runtime.name") + "\n");
         System.out.println("Java runtime version: " + System.getProperty("java.runtime.version"));
+        printLine();
     }
 
     // Info about CPU
     public void printCpuInfo() {
         Runtime runtime = Runtime.getRuntime();
 
-        System.out.println("===CPU INFO===");
+        System.out.println();
+        printLine();
+        System.out.printf("===CPU INFO===\n");
         int cores = Runtime.getRuntime().availableProcessors();
-        System.out.printf("Count of cores" + cores);
+        System.out.println("Count of cores: " + cores);
 
         // load average for last min
         OperatingSystemMXBean osBean = ManagementFactory.getOperatingSystemMXBean();
@@ -40,5 +44,10 @@ public class HardwareAnalyzer {
         } else {
             System.out.println("Avarage load is unavailable");
         }
+        printLine();
+    }
+
+    public void printLine() {
+        System.out.println("__________________________________________");
     }
 }
